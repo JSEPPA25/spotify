5ee2745e8bd3d7585e94c93f9a388867c9765340
JSEPPA25 <jacob.seppa@u.boisestate.edu>
2024-05-12 13:14:13 -0600

    Finished Parse and SQL insert

--- driver.js
+++ driver.js
@@ -1,86 +1,122 @@
 const e = require('express');
 const fs = require('fs');
 var mysql = require('mysql');
+const { title } = require('process');
 
+console.log("Initializing data structures")
 
-var con = mysql.createConnection({
+//Hash maps to store song data and history
+var songs = []
+var totalPlays = 0
+var history = []
+
+var con = mysql.createConnection({ //SQL connection variables
     user: "sonar",
     password: "vibe",
     server: "localhost",
     database: "spotifydata",
 });
 
-con.connect(function (err) {
+con.connect(function(err) { //Connect to SQL server
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Connected!"); //Log connection success
 });
 
-var trackList = {}
-var artistList = {}
-var albumList = {}
-var trackNames = {}
-var totalPlays = 0
+//Search for file "data.json" in the data folder
+fs.readFile('data/data.json', 'utf8', (err, input) => { //Read in the file
+    if (err) { console.error(err); return; }
 
-fs.readFile('data/data.json', 'utf8', (err, input) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+    console.log("File found")
+    data = JSON.parse(input) //Parse the JSON data
 
-    data = JSON.parse(input)
+    console.log("Reading data from file")
+    data.forEach(song => { //Iterate over the data
+        if (song.spotify_track_uri == null)
+            return; //Skip if no URI
 
-    data.forEach(element => {
-
-        let uri = element.spotify_track_uri
-        if (uri == null) {
-            return;
-        }
-        totalPlays += 1
-        let id = uri.substring(14)
-        let track = element.master_metadata_track_name
-        let album = element.master_metadata_album_album_name
-        let artist = element.master_metadata_album_artist_name
-
-        datetime = seperate(element.ts)
-
-        insertTrack(id, track)
-        insert(artist, artistList)
-        insert(album, albumList)
-
-
+        addSong(song)
     });
-    console.log("Total plays: " + totalPlays)
-    console.log("Total songs: " + Object.keys(trackNames).length)
-    console.log("Total artists: " + Object.keys(artistList).length)
-    console.log("Total albums: " + Object.keys(albumList).length)
+
+    insertData()
+    apiCall()
 });
 
-
-
-function insert(id, list) {
-    if (id in list) {
-        list[id] += 1;
-        return;
-    }
-    list[id] = 1;
-}
-
-function insertTrack(id, title) {
-    if (id in trackList) {
-        trackList[id] += 1;
-        return;
-    }
-    trackList[id] = 1;
-    trackNames[id] = title;
-}
 
 function seperate(ts) {
-    let date = new Date(ts)
-    let year = date.getFullYear()
-    let month = date.getMonth()
-    let day = date.getDate()
-    let time = new Date(date.getTime() - date.getTimezoneOffset() * 60000).toISOString().split("T")[1].split(".")[0].split(":").slice(0, 2).join(":")
-    return [year, month, day, time]
+
+    // Convert UTC to Mountain Time
+
+    const utcDate = new Date(ts);
+    const options = { timeZone: "America/Denver", hour12: false };
+    const mountainDate = utcDate.toLocaleString("en-US", options);
+
+    // Extracting individual components
+    const year = utcDate.getFullYear();
+    const month = String(utcDate.getMonth() + 1).padStart(2, '0');
+    const day = String(utcDate.getDate()).padStart(2, '0');
+    const hours = String(utcDate.getHours()).padStart(2, '0');
+    const minutes = String(utcDate.getMinutes()).padStart(2, '0');
+    const seconds = String(utcDate.getSeconds()).padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
+
+    return formattedDate;
+
+
 }
 
+function addSong(song) {
+    totalPlays += 1 //Increment total plays
 
+    let id = song.spotify_track_uri
+
+    //Remove special characters from the song name
+    let title = song.master_metadata_track_name.replaceAll("'", "''")
+    let artist = song.master_metadata_album_artist_name.replaceAll("'", "''")
+    let album = song.master_metadata_album_album_name.replaceAll("'", "''")
+
+    if (!(id in songs)) {
+        songs[id] = title
+        songs.push({ id: id, title: title, artist: artist, album: album })
+            // history.push({ id: id, timestamp: title, artist: artist, album: album })
+    }
+
+    history.push({ id: id, timestamp: seperate(song.ts), play_amount: song.ms_played, song_number: totalPlays });
+
+}
+
+function insertData() {
+
+    //SONGS TABLE
+    var sql = "TRUNCATE TABLE songs";
+    con.query(sql, function(err, result) { //Clears the table to prevent duplicates
+        if (err) throw err;
+        console.log("songs table emptied");
+    });
+
+    var sql = "INSERT INTO songs (id, name, album, artist) VALUES ?";
+    var values = songs.map(({ id, title, album, artist }) => [id, title, album, artist]) //Map the data to the correct columns
+    con.query(sql, [values], function(err, result) { //Insert the data in bulk
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
+
+
+    //HISTORY TABLE
+    var sql = "TRUNCATE TABLE history";
+    con.query(sql, function(err, result) { //Clears the table to prevent duplicates
+        if (err) throw err;
+        console.log("history table emptied");
+    });
+
+    var sql = "INSERT INTO history (id, timestamp, play_amount, song_number) VALUES ?";
+    var values = history.map(({ id, timestamp, play_amount, song_number }) => [id, timestamp, play_amount, song_number]) //Map the data to the correct columns
+    con.query(sql, [values], function(err, result) { //Insert the data in bulk
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
+}
+
+function apiCall() {
+
+}
